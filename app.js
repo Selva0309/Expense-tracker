0e25f3a4225ffa41b51ba74b39bf28495342e9b4
Selva0309 <97260536+Selva0309@users.noreply.github.com>
2022-11-20 04:42:47 +0530

    Expense tracker with ejs

---
+++ app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const sequelize = require('./utils/database');
+const Expenses = require('./model/Data');
+const expenseroute = require('./routes/expenseroute');
+
+
+const express = require('express');
+// const sequelize = require('./util/database');
+
+const app = express();
+app.use(express.static(path.join(__dirname,'css')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/expenselist', expenseroute);
+app.post('/expenses', expenseroute);
+
+
+
+app.use('/', expenseroute);
+
+
+sequelize.sync()
+.then(result=>{
+console.log('TABLE CREATED');
+app.listen(5000);
+})
+.catch(err=>console.log(err));
+
